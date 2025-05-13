841f2ea82fb93da49a1db06b94afa1e3abba416e
tizzifona <tizzifona@gmail.com>
2025-05-13 10:28:06 +0200

    Added code for Human.java

---
+++ src/main/java/org/femcoders/models/Human.java
@@ -0,0 +1,17 @@
+package org.femcoders.models;
+
+public class Human extends Character implements Fighter {
+    public Human(String name, int age, String race) {
+        super(name, age, "Human");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " says: For Gondor!");
+    }
+
+    @Override
+    public void attack() {
+        System.out.println(getName() + " swings a sword fiercely.");
+    }
+}
